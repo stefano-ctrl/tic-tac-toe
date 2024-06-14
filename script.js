de59fb90007641683bab2f1a8408a8b37c118559
Stefano <stefanopinna92@hotmail.it>
2024-06-14 01:16:38 +0100

    All logic in place and working on console

--- script.js
+++ script.js
@@ -65,7 +65,9 @@ gameArr = ['tl','t','tr', 'cl','c','cr', 'bl','b','br'];
            
        */
 
-       function playGame(playerOne, playerTwo) {
+       function alternateGame(playerOne, playerTwo) {
+
+   
     
          let playerPick = prompt('enter your choice', '');
          for (let i = 0; i < gameArr.length; i++) {
@@ -74,14 +76,12 @@ gameArr = ['tl','t','tr', 'cl','c','cr', 'bl','b','br'];
              if (findO.length < findX.length && gameArr[i] == playerPick) {
                 gameArr[i] = playerTwo.marker; 
                 console.log(gameArr);
-                console.log(findO < findX);
-                console.log(findX);
+                
              }
              else if (findO != findX && gameArr[i] == playerPick) {
                gameArr[i] = playerOne.marker; 
                console.log(gameArr); 
-               console.log(findO);
-               console.log(findX);
+               
              }
          }
        }
@@ -110,15 +110,70 @@ gameArr = ['tl','t','tr', 'cl','c','cr', 'bl','b','br'];
      with two different markers */
 
 
-  function startGame(game, playerOne, playerTwo) {
+  function startGame(gameBoard, playerOne, playerTwo) {
      let valueCheck = (currentValue) => currentValue == playerOne.marker || currentValue == playerTwo.marker; 
-    
-      if (game.every(valueCheck) != true) {
+
+     if(gameBoard[0] == playerOne.marker && gameBoard[1] == playerOne.marker && gameBoard[2] == playerOne.marker) {
+             return console.log('player one wins');
+
+  }    else if (gameBoard[0] == playerTwo.marker && gameBoard[1] == playerTwo.marker && gameBoard[2] == playerTwo.marker) {
+            return console.log('playerTwo wins'); 
+
+  }    else if(gameBoard[3] == playerOne.marker && gameBoard[4] == playerOne.marker && gameBoard[5] == playerOne.marker) {
+            return console.log('player one wins');
+
+}     else if (gameBoard[3] == playerTwo.marker && gameBoard[4] == playerTwo.marker && gameBoard[5] == playerTwo.marker) {
+  return console.log('playerTwo wins'); 
+} 
+
+      else if(gameBoard[6] == playerOne.marker && gameBoard[7] == playerOne.marker && gameBoard[8] == playerOne.marker) {
+   return console.log('player one wins');
+
+}      else if (gameBoard[6] == playerTwo.marker && gameBoard[7] == playerTwo.marker && gameBoard[8] == playerTwo.marker) {
+return console.log('playerTwo wins'); 
+
+}       else if(gameBoard[0] == playerOne.marker && gameBoard[3] == playerOne.marker && gameBoard[6] == playerOne.marker) {
+   return console.log('player one wins');
+
+}   else if (gameBoard[0] == playerTwo.marker && gameBoard[3] == playerTwo.marker && gameBoard[6] == playerTwo.marker) {
+return console.log('playerTwo wins'); 
+} 
+      else if(gameBoard[1] == playerOne.marker && gameBoard[4] == playerOne.marker && gameBoard[7] == playerOne.marker) {
+   return console.log('player one wins');
+
+}   else if (gameBoard[1] == playerTwo.marker && gameBoard[4] == playerTwo.marker && gameBoard[7] == playerTwo.marker) {
+return console.log('playerTwo wins'); 
+} 
+else if(gameBoard[2] == playerOne.marker && gameBoard[5] == playerOne.marker && gameBoard[8] == playerOne.marker) {
+   return console.log('player one wins');
+
+}   else if (gameBoard[1] == playerTwo.marker && gameBoard[5] == playerTwo.marker && gameBoard[8] == playerTwo.marker) {
+return console.log('playerTwo wins'); 
+} 
+
+else if(gameBoard[0] == playerOne.marker && gameBoard[4] == playerOne.marker && gameBoard[8] == playerOne.marker) {
+   return console.log('player one wins');
+
+}   else if (gameBoard[0] == playerTwo.marker && gameBoard[4] == playerTwo.marker && gameBoard[8] == playerTwo.marker) {
+return console.log('playerTwo wins'); 
+} 
+
+else if(gameBoard[2] == playerOne.marker && gameBoard[4] == playerOne.marker && gameBoard[6] == playerOne.marker) {
+   return console.log('player one wins');
+
+}   else if (gameBoard[2] == playerTwo.marker && gameBoard[4] == playerTwo.marker && gameBoard[6] == playerTwo.marker) {
+return console.log('playerTwo wins'); 
+} 
+        
+
+     else if (gameBoard.every(valueCheck) != true) {
           
-          playGame(playerOne, playerTwo); 
-          startGame(game, playerOne, playerTwo); 
+          alternateGame(playerOne, playerTwo); 
+          startGame(gameBoard, playerOne, playerTwo); 
           
-     } else if (game.every(valueCheck) == true) {
+     } 
+     
+     else if (gameBoard.every(valueCheck) == true) {
       console.log('game is finished');
       return
   } 
@@ -126,6 +181,10 @@ gameArr = ['tl','t','tr', 'cl','c','cr', 'bl','b','br'];
 
 
 
+
+
 startGame(gameArr, player1, player2); 
 
+
+// we have got all the logic in place. next thing to do is to render this on the dom. So substitute playerPick with events and need a function to manage this. All spots on grid needs to be interactive
 
