a8368a95af9bac3ba88c9c8d242ab4b3fae8f9ed
stefano-ctrl <77571388+stefano-ctrl@users.noreply.github.com>
2024-06-24 14:42:13 +0100

    Add files via upload

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -5,6 +5,13 @@ let gameArrChilds = document.querySelector('.grid-container').childNodes;
 let gameArrayFrom = Array.from(gameArrChilds); 
 let finalArray = gameArrayFrom.filter((element) => element.nodeName != '#text');
 
+let playerSelection = document.querySelectorAll('.players-markers'); 
+
+let replayBtn = document.querySelector('.btn-replay'); 
+
+
+
+//play against a friend 
 
 const createPlayer = function (name, marker) {
     this.name = name; 
@@ -18,11 +25,9 @@ const player1 = createPlayer('Kobe', 'X');
 const player2 = createPlayer('Shaq', 'O'); 
 
 
-
-   
-          
-          
-                 
+/*
+ 
+       // function two players 
    
          function alternateGame(playerOne, playerTwo) {
        
@@ -87,7 +92,7 @@ const player2 = createPlayer('Shaq', 'O');
 
 
         alternateGame(player1, player2); 
-   
+        
 
         
         
@@ -97,7 +102,7 @@ const player2 = createPlayer('Shaq', 'O');
          } else if(findX != findO) {
             playGame(playerOne); 
          }
-       }  */ 
+       }  */
         
      
        // condition should be if all arr[i] equals 'X' loops finishes)
@@ -117,7 +122,7 @@ const player2 = createPlayer('Shaq', 'O');
     
 
 // we have got all the logic in place. next thing to do is to render this on the dom. So substitute playerPick with events and need a function to manage this. All spots on grid needs to be interactive
-
+/*
 let winner; 
 function declareWinner() {
    return winner = true
@@ -231,25 +236,135 @@ else if(gameBoard[2] == playerOne.marker && gameBoard[4] == playerOne.marker && 
 
 
 // look how to disable touch event once player wins. go through logic, double check it 
-
 //after how to integrate pictures to game .
 
+//priorities _- restart game - declare winner (line that crosse all x or o) - game over - pick player
+   
+// make computer play against you       function getRandomInt(max) {return Math.floor(Math.random() * max);}
+//tidy up code, put it inside modules 
+// make player pick marker 
 // next tasks = create button to restart game
 // add a line that crosses line to show who's winning
 // create score board
 // improve css : take borders away grid - player one X and player two O should have a name on the side indicating what marker they are using 
 // show an interactive notification to show one players won 
 
- checkLogic(finalArray, player1, player2); 
+ // checkLogic(finalArray, player1, player2); 
 
 
 
+ //play against computer 
+
+ /*
+
+ function getRandomPlay(max, arr) {
+   let randomNum = Math.floor(Math.random() * max);
+   return arr[randomNum]; 
+}
+
+*/
+
+// function pickPlayer() two options, if player clicks player one will use x mark and start first 
+ // if player picks O mark will start second and computer first
+ // player it's picked by clicking 
+ // two divs rapresenting player and marker 
+ // 
+
+ 
+
+ 
+          /*
+        function pickPlayer(playerOne, playerTwo)  {
+         playerSelection.addEventListener('click', function(event) {
+            let markerPick = event.target; 
+            if (markerPick.className == "player-one") {
+               
+               // player one first to play and marker is X computer starts second
+            } else if (markerPick.className =="player-two"){
+               //player two plays second and marker is O. computer starts first  
+            }
+         })
+        }    */
+            
+             
+        // you need to have two systems for when player one is a computer and one for when 2 plays as computer 
 
 
 
+// play against computer
+
+/*
+function playComputer(playerOne, playerTwo) {
+   let computerPoints = []; 
+   let playerPoints = []; 
+   
+
+}
+
+*/
+
+// set game and alternate between playerone and computer 
+let gameArr = []; 
+let arrX = [];
+let arrO = []; 
+
+/*
+function computerPlay(arr, playerTwo) {
+   let isEqualO = (value) => value == playerTwo.marker; 
+ 
+   if(gameArr.every(isEqualO) && gameArr.length > 9) {
+     
+      return; 
+   }  else if (gameArr.length < 9){ 
+      
+   let randomArray = arr[Math.floor(Math.random() * arr.length)];
+  
+   let player2O = new Image(); 
+   player2O.src = 'img/circle.svg'; 
+
+   randomArray.appendChild(player2O); 
+    gameArr.push(randomArray);
+    let newArr = arr.filter((value) => value != randomArray); 
+
+   randomArray = playerTwo.marker; 
+   
+
+   console.log(!arr.every(isEqualO) && arr.length < 9); 
+   computerPlay(newArr, playerTwo);
+} 
 
 
+}
+
+computerPlay(finalArray, player2); 
+
+*/
+
+function playerVsComputer(arr, playerTwo) {
+   let isEqualO = (value) => value == playerTwo.marker; 
+ 
+   if(gameArr.every(isEqualO) && gameArr.length > 9) {
+     
+      return; 
+   }  else if (gameArr.length < 9){ 
+      
+   let randomArray = arr[Math.floor(Math.random() * arr.length)];
+  
+   let player2O = new Image(); 
+   player2O.src = 'img/circle.svg'; 
+
+   randomArray.appendChild(player2O); 
+    gameArr.push(randomArray);
+    let newArr = arr.filter((value) => value != randomArray); 
+
+  arrO.push(pal)
+   
+
+   console.log(!arr.every(isEqualO) && arr.length < 9); 
+   playerVsComputer(newArr, playerTwo);
+} 
 
 
+}
 
 
